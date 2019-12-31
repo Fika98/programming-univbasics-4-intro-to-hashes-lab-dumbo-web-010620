307a27eb6715bca9c47b0a149a18607041eaf5f5
Firuz Afzalov <afzalov98@gmail.com>
2019-12-31 22:43:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,6 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  hash[key]
+  if hash
 end
-h = {a: 4, b:12}
-h[:a] = 32
-update_counting_hash(h, :a)
+
